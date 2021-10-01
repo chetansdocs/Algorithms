d89b1319134f020d28352958a18d9d14fb1778bc
Chetan Shirsath <chetan_shirsath@persistent.com>
2021-10-01 17:39:28 +0530

    String permutations

--- src/main/java/org/example/strings/StringPermutations.java
+++ src/main/java/org/example/strings/StringPermutations.java
@@ -2,7 +2,26 @@ package org.example.strings;
 
 public class StringPermutations{
 
-    public static void main(String[] args) {
+    private static void printPermutations(String inputString, int startIndex, int endIndex) {
+        if(startIndex == endIndex) {
+            System.out.println(inputString);
+        } else {
+            for(int i = startIndex; i <= endIndex; i++) {
+                String modifiedStr = swapPositions(inputString, startIndex, i);
+                printPermutations(modifiedStr, startIndex + 1, endIndex);
+                inputString = swapPositions(modifiedStr, startIndex, i);
+            }
+        }
+    }
 
+    private static String swapPositions(String str, int index1, int index2) {
+        StringBuilder sb = new StringBuilder(str);
+        sb.setCharAt(index1, str.charAt(index2));
+        sb.setCharAt(index2, str.charAt(index1));
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        printPermutations("ABCD",0, 3);
     }
 }
